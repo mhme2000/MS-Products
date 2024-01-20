5074c5a952a85ab1c34a6a26387d3705071e676d
Marcos Eckart <Marcos Eckart@ECKART>
2024-01-20 15:33:37 -0300

    Implement elasticache

--- MS-Products.Application/Services/ProductService.cs
+++ MS-Products.Application/Services/ProductService.cs
@@ -1,39 +1,74 @@
-﻿using MS_Products.Application.Interfaces;
-using MS_Products.Domain.Entities;
+﻿using MS_Products.Domain.Entities;
 using MS_Products.Domain.Interfaces;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
 namespace MS_Products.Application.Services;
-public class ProductService(IProductRepository repository) : IProductService
+public class ProductService
 {
-    private readonly IProductRepository _repository = repository;
-    public Task CreateProduct(Product product)
+    private readonly IProductRepository _repository;
+    private readonly IDatabase _cache;
+    private readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("elasticache-rg2n57.serverless.use1.cache.amazonaws.com:6379");
+
+    public ProductService(IProductRepository repository)
     {
-        return _repository.CreateProduct(product);
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _cache = redis.GetDatabase();
     }
 
-    public Task DeleteProductById(Guid productId)
+    public async Task CreateProduct(Product product)
     {
-        return _repository.DeleteProductById(productId);
+        await _repository.CreateProduct(product);
+        ClearCache();
     }
 
-    public Task<IEnumerable<Product>> GetAllProducts()
+    public async Task DeleteProductById(Guid productId)
     {
-        return _repository.GetAllProducts();
+        await _repository.DeleteProductById(productId);
+        ClearCache();
     }
 
-    public Task<List<Product>> GetProductByCategoryId(Guid categoryId)
+    public async Task<IEnumerable<Product>> GetAllProducts()
     {
-        return _repository.GetProductByCategoryId(categoryId);
+        var cachedData = await _cache.StringGetAsync("AllProducts");
+        if (!cachedData.IsNullOrEmpty)
+            return JsonConvert.DeserializeObject<IEnumerable<Product>>(cachedData);
+        var products = await _repository.GetAllProducts();
+        await _cache.StringSetAsync("AllProducts", JsonConvert.SerializeObject(products));
+
+        return products;
+    }
+
+    public async Task<List<Product>> GetProductByCategoryId(Guid categoryId)
+    {
+        var products = await _repository.GetProductByCategoryId(categoryId);
+        return products;
     }
 
     public async Task<Product> GetProductById(Guid productId)
     {
-        var info = await _repository.GetProductById(productId);
-        return info ?? throw new Exception("Produto não encontrado.");
+        var cachedData = await _cache.StringGetAsync($"Product:{productId}");
+        if (!cachedData.IsNullOrEmpty)
+            return JsonConvert.DeserializeObject<Product>(cachedData);        
+        var product = await _repository.GetProductById(productId);
+        if (product != null)
+        {
+            await _cache.StringSetAsync($"Product:{productId}", JsonConvert.SerializeObject(product));
+            return product;
+        }
+        throw new Exception("Produto não encontrado.");
     }
 
     public async Task UpdateProductById(Product product)
     {
-        await GetProductById(product.Id);      
+        await GetProductById(product.Id);
         await _repository.UpdateProductById(product);
+        ClearCache(product.Id);
+    }
+
+    private void ClearCache(Guid? productId = null)
+    {
+        if(productId != null)_cache.KeyDeleteAsync($"Product:{productId}");
+        _cache.KeyDelete("AllProducts");
     }
 }
